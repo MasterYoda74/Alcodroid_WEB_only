769ea247d5d2af7f605b239b81f8a006a79f71ee
MasterYoda74 <ayup27@bk.ru>
2019-01-06 17:32:01 +0500

    Safe

--- js/func.js
+++ js/func.js
@@ -80,8 +80,109 @@ function SendRequest(r_method, r_path, r_args, r_handler)
     }
 } 
 
-function test(test) {
-    alert(JSON.parse(test));
+function parseconfig(jsonobj) {
+    //alert(jsonobj.responseText);
+    var configJ = JSON.parse(jsonobj.responseText);
+    if (configJ.isAP) {
+        document.getElementById("isAP").setAttribute("checked","checked");
+    } else {
+        document.getElementById("isAP").removeAttribute("checked");
+    }    
+    document.getElementById("APSSID").value=configJ.APSSID;
+    document.getElementById("APPASS").value=configJ.APPASS;
+    document.getElementById("wifiSSID").value=configJ.wifiSSID;
+    document.getElementById("wifiPASS").value=configJ.wifiPASS;
+    document.getElementById("SMAX").value=configJ.servoMAX;
+    document.getElementById("SMIN").value=configJ.servoMIN;
+    document.getElementById("Steps").value=configJ.stepPerMl;
+    document.getElementById("Start").value=configJ.startMl;
+    document.getElementById("Feedback").value=configJ.feedback;
+    document.getElementById("LED").value=configJ.ledbright;
 }
-
-SendRequest("GET","config.json","",test);
+function parseshots(jsonobj) {
+    //alert(jsonobj.responseText);
+    var configJ = JSON.parse(jsonobj.responseText);
+    //alert(configJ.shots.length);
+    var parent=document.getElementById("shots");
+    for (i=0; i<configJ.shots.length; i++) {
+        var newline=document.createElement('div');
+        newline.className="container-fluid";
+        var div1=document.createElement('div');
+        div1.className="col-xs-6";
+        var div2=document.createElement('div');
+        div2.className="col-xs-4";
+        var div3=document.createElement('div');
+        div3.className="col-xs-2";        
+        var newinput=document.createElement('div');
+        newinput.className="form-control";
+        //newinput.style="width: 40%;";
+        //newinput.pattern="[0-9a-zA-Zа-яА-Я.\- ]{1,20}";
+        newinput.innerText=configJ.shots[i].ID;
+        div1.appendChild(newinput);
+        newline.appendChild(div1);
+        var newdoze=document.createElement('div');
+        newdoze.className="form-control";
+        newdoze.style="background-color: "+configJ.shots[i].color+";";
+        div2.appendChild(newdoze);
+        newline.appendChild(div2);
+        var newbutt=document.createElement('div');
+        newbutt.className="btn btn-block btn-success btn-plus";
+        //newbutt.style="width: 25%;";
+        newbutt.innerText="+";
+        div3.appendChild(newbutt);
+        newline.appendChild(div3);
+        parent.insertBefore(newline,document.getElementById("new-shot"));
+    }
+}
+function parseusers(jsonobj) {
+    alert(jsonobj.responseText);
+    var configJ = JSON.parse(jsonobj.responseText);
+    alert(configJ.users.length);
+    var parent=document.getElementById("users");
+    for (i=0; i<configJ.users.length; i++) {
+        var newline=document.createElement('div');
+        newline.className="container-fluid no-gutters";
+        var div1=document.createElement('div');
+        div1.className="col-xs-12 id-num";
+        div1.innerHTML="ID: <span>"+configJ.users[i].ID+"</span>";
+        var div2=document.createElement('div');
+        div2.className="col-xs-12 col-sm-4";
+        var inpname=document.createElement('input');
+        inpname.className="form-control";
+        inpname.placeholder="Имя";
+        inpname.value=configJ.users[i].name;
+        div2.appendChild(inpname);
+        var div3=document.createElement('div');
+        div3.className="col-xs-5 col-sm-3"; 
+        var newcolor=document.createElement('div');
+        newcolor.className="form-control";
+        newcolor.style="background-color: "+configJ.users[i].color+";";
+        div3.appendChild(newcolor);
+        var div4=document.createElement('div');
+        div4.className="col-xs-5 col-sm-3";
+        var newdoze=document.createElement('input');
+        newdoze.type="number";
+        newdoze.className="form-control";
+        newdoze.placeholder="Доза";
+        newdoze.min="5";
+        newdoze.max="50";
+        newdoze.step="5";
+        newdoze.value=configJ.users[i].doze;
+        div4.appendChild(newdoze);
+        var div5=document.createElement('div');
+        div5.className="col-xs-2 col-sm-2";
+        var newbutt=document.createElement('div');
+        newbutt.className="btn btn-block btn-danger btn-minus";
+        newbutt.innerText="-";
+        div5.appendChild(newbutt);
+        newline.appendChild(div1);
+        newline.appendChild(div2);
+        newline.appendChild(div3);
+        newline.appendChild(div4);
+        newline.appendChild(div5);
+        parent.appendChild(newline);
+    }
+}
+SendRequest("GET","config.json","",parseconfig);
+SendRequest("GET","shots.json","",parseshots);
+SendRequest("GET","users.json","",parseusers);
